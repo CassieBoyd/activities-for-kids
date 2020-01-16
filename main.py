5314a67b4879e6dae9f2af5fad39d24a756009e3
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-16 11:22:44 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Define four Python functions named run, swing, slide, and hide_and_seek. Each function should take a child's name as an argument. Each function should print that the child performed the activity.
+
+For example, Jay ran like a fool! or Chantelle slid down the slide!.
+"""
+
+const run = (kid) => {
+    print(f"Run, {kid}, run!")
+}
+
+const swing = (kid) => {
+    print(f"{kid} is swingin' in the rain, just swingin' in the rain")
+}
+
+def slide(kid):
+    print(f"{kid} broke on through to the other slide!")
+
+def hide_and_seek(kid):
+    print(f"Ready or not, {kid} will find you and they will kill you!")
+
+"""
+The following lists of children should be iterated, and the names sent to the appropriate functions.
+"""
+
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+for kid in running_kids:
+    run(kid)
